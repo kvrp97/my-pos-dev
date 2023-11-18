3813d63418ca4863d5cc160f92cdd6bbbebe36f0
kvrp97 <ravinduprabhashwara97@gmail.com>
2023-11-18 07:08:28 +0530

    Initial Commit

---
+++ src/com/devstack/pos/entity/UserRole.java
@@ -0,0 +1,28 @@
+package com.devstack.pos.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRole implements SuperEntity{
+    @Id
+    @Column(name = "property_id")
+    private Integer propertyId;
+    @Column(name = "role_name", nullable = false)
+    private String roleName;
+    @Column(name = "role_description", nullable = false)
+    private String roleDescription;
+
+    @OneToMany(mappedBy = "userRole")
+    private List<User> users;
+}
